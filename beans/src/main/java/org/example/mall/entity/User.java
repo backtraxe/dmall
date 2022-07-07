ca51f244612d907e93c4ccfe3fdd5beda36399ba
backtraxe <backtraxe@gmail.com>
2022-07-07 11:42:52 +0800

    add swagger dependencies

--- beans/src/main/java/org/example/mall/entity/User.java
+++ beans/src/main/java/org/example/mall/entity/User.java
@@ -1,5 +1,7 @@
 package org.example.mall.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,15 +11,21 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@ApiModel
 public class User {
 
+    @ApiModelProperty(value = "用户ID", name = "userID", dataType = "int")
     private int userID;
 
+    @ApiModelProperty(value = "用户名", name = "username", dataType = "String")
     private String userName;
 
+    @ApiModelProperty(value = "密码", name = "password", dataType = "String")
     private String userPwd;
 
+    @ApiModelProperty(value = "真实姓名", name = "userRealName", dataType = "String")
     private String userRealName;
 
+    @ApiModelProperty(value = "用户头像", name = "userIMG", dataType = "String")
     private String userImg;
 }
--- common/src/main/java/org/example/mall/vo/ResultVO.java
+++ common/src/main/java/org/example/mall/vo/ResultVO.java
@@ -1,5 +1,7 @@
 package org.example.mall.vo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,14 +9,18 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel
 public class ResultVO {
 
     // 响应前端的状态码
+    @ApiModelProperty()
     private int code;
 
     // 响应前端的提示信息
+    @ApiModelProperty()
     private String msg;
 
     // 响应前端的数据
+    @ApiModelProperty()
     private Object data;
 }
